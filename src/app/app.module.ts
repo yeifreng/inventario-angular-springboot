00bcd9e0b98e453dd6747bb30387df7868f34e39
Yeifreng <yeifrenjose@gmail.com>
2024-01-04 13:41:12 -0500

    creando componente de menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ListadoProductoComponent } from './listado-producto/listado-producto.component';
 import { ListadoProveedorComponent } from './listado-proveedor/listado-proveedor.component';
 import { ListadoCategoriaComponent } from './listado-categoria/listado-categoria.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListadoProductoComponent,
     ListadoProveedorComponent,
-    ListadoCategoriaComponent
+    ListadoCategoriaComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
